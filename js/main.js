abf360fdbe9d35eeca42cc142ef05b31d4c7866d
root <root@localhost.localdomain>
2017-08-22 11:20:10 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-(function ($) {
-	
-	/*============== JS FILE ACTIVE ===============*/
-	/*-----------------------------------------
-	[1. jQuery Mobilemenu ]
-	[2. Bootstrap Tabs ]
-	[3. ScrollUp ]
-	[4. Google Map ]
-	--------------------------------------------*/
-	/*---------------------
-	 [1. jQuery Mobilemenu ]
-	--------------------- */
-		jQuery('nav#dropdown').meanmenu();
-		
-	/*---------------------
-	[2. Bootstrap Tabs ]
-	--------------------- */
-	$('#myTabs a').on('click',function (e) {
-		e.preventDefault()
-		$(this).tab('show')
-	});
-	/*-------------------
-	[3. ScrollUp ]
-	---------------------*/
-	$.scrollUp({
-		animation: 'slide', // Fade, slide, none
-		scrollSpeed: 1500,
-		scrollText: [
-		  "<i class='fa fa-chevron-up'></i>"
-		]
-	});
-	
-})(jQuery);
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+},{}]},{},[1])
